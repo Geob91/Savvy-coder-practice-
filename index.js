3af7a13e3b1c4a2346acba7e385d123eab37cc17
George Blezard <gblezard91@gmail.com>
2019-11-05 21:05:56 -0600

    add objects

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const myName = "George Blezard";
+
+console.log(`Hello World this is ${myName}`);
+
+
+const me = {
+
+  name: "George",
+  age: 28,
+  job: "unemployed",
+  isFoodNice: true,
+  animals: "none",
+
+};
+
+//BRACKET NOTATION
+console.log(me["name"]);
+
+// DOT NOTATION
+console.log(me.age);
+
+
