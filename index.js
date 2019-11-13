cd35eb2ee896a1fd12f90f9847e58440709161ab
George Blezard <gblezard91@gmail.com>
2019-11-12 20:53:48 -0600

    add multiple fuctions

--- index.js
+++ index.js
@@ -15,8 +15,48 @@ function add(x, y) {
 console.log(doMath(10, 5, subtract));
 console.log(doMath(10, 5, add));
 
+// function factory below
+
 function initSportScorer(pts) {
   return function(score) {
     return score + pts;
   };
 }
+const threePointer = initSportScorer(3);
+console.log(threePointer(99));
+
+// Making my own function
+
+function rugbyScore(pts) {
+  return function(score) {
+    return score + pts;
+  };
+}
+const fivePoints = rugbyScore(5);
+console.log(fivePoints(25));
+
+// Call stack
+
+function foo() {
+  console.trace();
+  bar();
+  console.trace();
+
+  return "foo";
+}
+
+function bar() {
+  console.trace();
+  baz();
+  console.trace();
+
+  return "bar";
+}
+
+function baz() {
+  console.trace();
+
+  return "baz";
+}
+
+console.log(foo());
