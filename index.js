ec9773bf4835812d91838811fab0b50f92310129
George Blezard <gblezard91@gmail.com>
2019-11-12 18:58:02 -0600

    add index new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const myName = "George Blezard";
+/*const myName = "George Blezard";
 
 console.log(`Hello World this is ${myName}`);
 
@@ -20,4 +20,44 @@ console.log(me["name"]);
 // DOT NOTATION
 console.log(me.age);
 
+const numberSummerMachine = {
+
+  startingNumber: 0,
+  endingNumber: 10,
+
+let sumDaNumbers = 0;
+
+    for (let i = 1, i<= 10, i+=1){
+    sumDaNumbers += i;
+    return this.sumDaNumbers§;
+
+    }
+  }
+
+console.log(sumDaNumbers());
+
+*/
+
+
+
+
+ const summmerNumberMachine ={
+  startingNumber = 1,
+  endingNumber = 100,
+
+
+  sumDaNumber: function(startingNumber, endingNumber){
+
+  let sumDaNumber = 1,
+
+  for (let i = startingNumber ; i < endingNumber ; i += 1){
+
+    sumDaNumber = i
+
+  }
+
+  return sumDaNumber
+}
+}
+console.log(numberSummerMachine.sumDaNumber(numberSummerMachine.startingNumber,numberSummerMachine.endingNumber))
 
