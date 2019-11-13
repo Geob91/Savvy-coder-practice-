c7fc3d74406ac8d41ea7584a6779a004b33b0d57
George Blezard <gblezard91@gmail.com>
2019-11-12 21:20:25 -0600

    add logical and comparison

--- index.js
+++ index.js
@@ -1,62 +1,12 @@
-function doMath(x, y, operation) {
-  console.trace();
-  return operation(x, y);
-}
-function subtract(x, y) {
-  console.trace();
-  return x - y;
-}
+const x = 3,
+const y = 5,
+const z = 10,
 
-function add(x, y) {
-  console.trace;
-  return x + y;
-}
 
-console.log(doMath(10, 5, subtract));
-console.log(doMath(10, 5, add));
+// logical operators involve AND and OR CONDITIONS
+//&& - AND both left and right operands must be true
+console.log(x < y && x < z);
 
-// function factory below
+// || - OR if left operand is true right is never evaluated
 
-function initSportScorer(pts) {
-  return function(score) {
-    return score + pts;
-  };
-}
-const threePointer = initSportScorer(3);
-console.log(threePointer(99));
-
-// Making my own function
-
-function rugbyScore(pts) {
-  return function(score) {
-    return score + pts;
-  };
-}
-const fivePoints = rugbyScore(5);
-console.log(fivePoints(25));
-
-// Call stack
-
-function foo() {
-  console.trace();
-  bar();
-  console.trace();
-
-  return "foo";
-}
-
-function bar() {
-  console.trace();
-  baz();
-  console.trace();
-
-  return "bar";
-}
-
-function baz() {
-  console.trace();
-
-  return "baz";
-}
-
-console.log(foo());
+console.log(x < y || x < z);
