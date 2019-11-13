d2d2ae2e8d2fd95e4bcc1dfdad443d0fa111fead
George Blezard <gblezard91@gmail.com>
2019-11-12 20:30:21 -0600

    add function factory

--- index.js
+++ index.js
@@ -1,63 +1,12 @@
-
-/*const myName = "George Blezard";
-
-console.log(`Hello World this is ${myName}`);
-
-
-const me = {
-
-  name: "George",
-  age: 28,
-  job: "unemployed",
-  isFoodNice: true,
-  animals: "none",
-
-};
-
-//BRACKET NOTATION
-console.log(me["name"]);
-
-// DOT NOTATION
-console.log(me.age);
-
-const numberSummerMachine = {
-
-  startingNumber: 0,
-  endingNumber: 10,
-
-let sumDaNumbers = 0;
-
-    for (let i = 1, i<= 10, i+=1){
-    sumDaNumbers += i;
-    return this.sumDaNumbers§;
-
-    }
-  }
-
-console.log(sumDaNumbers());
-
-*/
-
-
-
-
- const summmerNumberMachine ={
-  startingNumber = 1,
-  endingNumber = 100,
-
-
-  sumDaNumber: function(startingNumber, endingNumber){
-
-  let sumDaNumber = 1,
-
-  for (let i = startingNumber ; i < endingNumber ; i += 1){
-
-    sumDaNumber = i
-
-  }
-
-  return sumDaNumber
+function initSportsScorer(pts) {
+  return function(score) {
+    return score + pts;
+  };
 }
-}
-console.log(numberSummerMachine.sumDaNumber(numberSummerMachine.startingNumber,numberSummerMachine.endingNumber))
 
+const threePointers = initSportsScorer(3);
+
+const touchdown = initSportsScorer(7);
+
+console.log(threePointers(99));
+console.log(touchdown(14));
