3be99102a3885a00392ed43a46edf77ed3c256cd
George Blezard <gblezard91@gmail.com>
2019-11-12 20:32:05 -0600

    add math function

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function initSportsScorer(pts) {
+function doMath(x, y, operation) {
+  console.trace();
+  return operation(x, y);
+}
+function subtract(x, y) {
+  console.trace();
+  return x - y;
+}
+
+function add(x, y) {
+  console.trace;
+  return x + y;
+}
+
+console.log(doMath(10, 5, subtract));
+console.log(doMath(10, 5, add));
+
+function initSportScorer(pts) {
   return function(score) {
     return score + pts;
   };
 }
-
-const threePointers = initSportsScorer(3);
-
-const touchdown = initSportsScorer(7);
-
-console.log(threePointers(99));
-console.log(touchdown(14));
