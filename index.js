3b8f9ebe25e671a1b78f82f0d77cc7084a192fb2
George Blezard <gblezard91@gmail.com>
2019-11-13 18:36:08 -0600

    Add practice functions

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const x = 3,
-const y = 5,
-const z = 10,
+// Multiple functions - function factory
 
+function initSportsScorer(pts) {
+  return function(score) {
+    return score + pts;
+  };
+}
 
-// logical operators involve AND and OR CONDITIONS
-//&& - AND both left and right operands must be true
-console.log(x < y && x < z);
+const threePointer = initSportsScorer(3);
+console.log(threePointer(99));
 
-// || - OR if left operand is true right is never evaluated
+//Word Function
 
-console.log(x < y || x < z);
+function wordMessage(greeting, name) {
+  return function(niceMessage) {
+    return greeting + name + niceMessage;
+  };
+}
+
+const morningMessage = wordMessage("Good Morning ", "Steve ");
+console.log(morningMessage("How are you this fine day?"));
+
+// harder function
+
+function rugbyPoints(pts) {
+  return function(finalScore) {
+    return pts + finalScore;
+  };
+}
+
+const winningGame = rugbyPoints(5);
+console.log(winningGame(112));
+
+const xtraPt = rugbyPoints(2);
+console.log(xtraPt(114));
