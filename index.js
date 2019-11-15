7c673e8754cc07a7588b9a0037276afe8a40c196
George Blezard <gblezard91@gmail.com>
2019-11-14 20:30:31 -0600

    add factorial

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-// 2 * 2 * 2
+function factorial (num) {
 
-console.log(Math.pow(2, 3));
+  let ret = 1;
 
-function myPow(num, exp){
-  let ret = num;
+  for (let i = num; i >= 1; i -= 1){
+    ret *= i;
 
-  for (let i = 1; i < exp; i += 1){
-    ret *= num;
   }
 
   return ret;
 }
 
-console.log(myPow(2, 3));
-
-// exp recursion
-
-function myPow(num, exp){
-  if (exp === 0) {
-    return 1;
-  }
-
-  return (num *= myPow(num, exp - 1));
-
-}
-
-console.log(myPow(2, 3))
+console.log(factorial(4));
