c2d4dfd45689c2339af72399917bd1ffd7ba5323
George Blezard <gblezard91@gmail.com>
2019-11-14 20:12:29 -0600

    Add class exercise loop and recursion

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function myPow(num, exp){
 }
 
 console.log(myPow(2, 3));
+
+// exp recursion
+
+function myPow(num, exp){
+  if (exp === 0) {
+    return 1;
+  }
+
+  return (num *= myPow(num, exp - 1));
+
+}
+
+console.log(myPow(2, 3))
