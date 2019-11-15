5d50f4654c2a085ced356c96a621202535a22c5c
George Blezard <gblezard91@gmail.com>
2019-11-14 21:02:51 -0600

    add pass by value

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function factorial (num) {
+// pass by reference and pass by value
 
-  if (num === 0){
-    return 1;
-  }
+let x = 3;
 
-  return (num *= factorial(num - 1));
+//Y references it's own copy of the number 3
+let y = x;
 
+function add1(n){
+  return n + 1;
 }
 
-console.log(factorial(4));
+//Primitives cannot be mutated
+//primitives are passed by value
+
+x = add1 (x);
+console.log("x after add1 is", x);
+console.log(y);
