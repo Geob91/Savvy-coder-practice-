6faa8b65918ef1954cace40609735be30a5576d6
George Blezard <gblezard91@gmail.com>
2019-11-15 21:04:12 -0600

    Add new array

---
+++ random-student.js
@@ -0,0 +1,22 @@
+/* Arrays auto-incremented numerical keys collections AKA composite data types
+ */
+
+const myArr = [
+  [
+    "Andrew",
+    "Billy",
+    "George",
+    "Heather",
+    "Madison",
+    "Marque",
+    "Martin",
+    "Nikki",
+    "Oscar",
+    "RhondaLyn",
+    "Sidney",
+    "Tracie",
+    "Wallat"
+  ]
+];
+
+console.log(myArr[0].length);
