3f3e746393d334ae07c2c4e4df86d92b07162d7a
George Blezard <gblezard91@gmail.com>
2019-11-15 21:15:58 -0600

    Add array random student code

--- random-student.js
+++ random-student.js
@@ -2,21 +2,25 @@
  */
 
 const myArr = [
-  [
-    "Andrew",
-    "Billy",
-    "George",
-    "Heather",
-    "Madison",
-    "Marque",
-    "Martin",
-    "Nikki",
-    "Oscar",
-    "RhondaLyn",
-    "Sidney",
-    "Tracie",
-    "Wallat"
-  ]
+  "Andrew",
+  "Billy",
+  "George",
+  "Heather",
+  "Madison",
+  "Marque",
+  "Martin",
+  "Nikki",
+  "Oscar",
+  "RhondaLyn",
+  "Sidney",
+  "Tracie",
+  "Wallat"
 ];
 
-console.log(myArr[0].length);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+console.log(myArr[getRandomInt(0, myArr.length - 1)]);
