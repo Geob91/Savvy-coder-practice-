7ee08ff68ec170e674578d974367ba5fcabb36c3
George Blezard <gblezard91@gmail.com>
2019-11-15 18:31:50 -0600

    Add fizz buzz function

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-//Pass by reference
-
-const me = {
-  name:"Mark"
-};
-
-const me2 = me;
-
-function nameChanger(obj, newName) {
-  obj.name =newName;
+function initFizzBuzz(num1, num2) {
+  for (let i = 0; i < 99; i++) {
+    if (i % num1 === 0 && i % num2 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % num1 === 0) {
+      console.log("Fizz");
+    } else if (i % num2 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log("Value of i is " + i);
+    }
+  }
 }
 
-nameChanger(me,'Fred');
+const fizzBuzz46 = initFizzBuzz(4, 6);
