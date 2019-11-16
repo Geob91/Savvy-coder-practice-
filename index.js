2c4d379241619c38b70e3fa44ddf9fb755440b33
George Blezard <gblezard91@gmail.com>
2019-11-15 18:45:05 -0600

    Add object project

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-function initFizzBuzz(num1, num2) {
-  for (let i = 0; i < 99; i++) {
-    if (i % num1 === 0 && i % num2 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % num1 === 0) {
-      console.log("Fizz");
-    } else if (i % num2 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log("Value of i is " + i);
+function initPerson(fname, lname, age) {
+  return {
+    fname,
+    lname,
+    age,
+    canDrink: function() {
+      return age >= 21;
     }
-  }
+  };
 }
 
-const fizzBuzz46 = initFizzBuzz(4, 6);
+const me = initPerson("George", "Blezard", 19);
+
+console.log(me.canDrink());
