06090c79ef0b579d4ee0ef30797fd372affa0025
George Blezard <gblezard91@gmail.com>
2019-11-17 15:08:00 -0600

    Add FizzBuzz function factory solution

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function logNumber(num) {
-  let count = 1;
-
-  while (count <= num) {
-    console.log(count);
-    count += 1;
-  }
+function initFizzBuzz(num1, num2) {
+  return function fizzBuzz46() {
+    for (let i = 1; i <= 100; i += 1) {
+      if (i % num1 === 0 && i % num2 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % num1 === 0) {
+        console.log("Fizz");
+      } else if (i % num2 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+    }
+    return initFizzBuzz();
+  };
 }
 
-logNumber(100);
+const fizzBuzz46 = initFizzBuzz(4, 6);
+
+fizzBuzz46();
