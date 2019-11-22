3355fb90c4a350d955d326e00534d32fc08d6683
George Blezard <gblezard91@gmail.com>
2019-11-22 13:07:19 -0600

    Add Zlatan constructor function

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const numbersAndStrings = [
-  12,
-  "ab",
-  3,
-  6,
-  "c",
-  90,
-  "web",
-  "dev",
-  1440,
-  "rusty"
-];
-
-for (let i = 0; i <= numbersAndStrings.length; i += 1) {
-  console.log(numbersAndStrings[i]);
+function Striker(first, last, age, internationalCaps, club) {
+  this.first = first;
+  this.last = last;
+  this.age = age;
+  this.internationalCaps = internationalCaps;
+  this.club = club;
 }
 
-const popped = numbersAndStrings.pop();
-console.log(numbersAndStrings);
-console.log(popped);
+Striker.prototype.fname = function() {
+  return `My name is ${this.first} it is your pleasure to meet me`;
+};
+
+Striker.prototype.lname = function() {
+  return `if you have ever met ${this.first} ${this.last} your welcome.`;
+};
+
+Striker.prototype.career = function() {
+  return `I am ${this.age} and have ${this.internationalCaps} for Sweden and currently play for ${this.club}`;
+};
+
+const theManTheMythTheLegend = new Striker(
+  "Zlatan",
+  "Ibrahimovic",
+  38,
+  116,
+  "LA Galaxy"
+);
+
+console.log(theManTheMythTheLegend);
+console.log(theManTheMythTheLegend.fname());
+console.log(theManTheMythTheLegend.lname());
+console.log(theManTheMythTheLegend.career());
