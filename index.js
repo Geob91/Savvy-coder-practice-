85b6fd8c0608724f1a4bc2e13f1b685f03ed895b
George Blezard <gblezard91@gmail.com>
2019-11-24 16:33:22 -0600

    Add Rugby Player with Setter and Getter and class

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-class Striker {
-  constructor(first, last, age, internationalCaps, club) {
-    this.first = first;
-    this.last = last;
-    this.age = age;
-    this.internationalCaps = internationalCaps;
-    this.club = club;
+class RugbyPlayer {
+  constructor(height, weight, speed, aggression) {
+    this.height = height;
+    this.weight = weight;
+    this.speed = speed;
+    this.aggression = aggression;
   }
 
-  get fname() {
-    return `My name is ${this.first} it is your pleasure to meet me.`;
+  get whatPosition() {
+    if (this.height > 6.2 && this.weight > 260 && aggression === true) {
+      return "Your a big mean dude I think you should be a forward";
+    }
+
+    return "Ok maybe we need to find something else for you";
   }
 
-  get lname() {
-    return `If you have ever met ${this.first} ${this.last} your welcome.`;
+  get speedPosition() {
+    if (this.speed > 20) {
+      return "Holy moly your fast we are putting you on the wing!";
+    }
+    return "OK so maybe your not a winger lets carry on";
   }
 
-  get career() {
-    return `I am ${this.age} and have ${this.internationalCaps} caps for Sweden and currently play for ${this.club}.`;
+  get backPosition() {
+    if (
+      this.height > 6.0 &&
+      this.speed > 12 &&
+      this.weight > 250 &&
+      this.aggression === true
+    ) {
+      return "I think we need to see you in action, you could be a bulldozer backline or a demon of a lose forward.";
+    }
   }
 
-  //set oldClubs(newArray) {
-  //   this.array = newArray;
-  // }
+  set newHeight(tallHeight) {
+    this.height = tallHeight;
+  }
+
+  get amazingPlayer() {
+    return "I can play anywhere";
+  }
 }
-const zlatan = new Striker("Zlatan", "Ibrahimovic", 38, 116, "LA Galaxy");
 
-zlatanArray = [
-  "Malmo",
-  "Ajax",
-  "Juventus",
-  "Inter Milan",
-  "Barcelona",
-  "PSG",
-  "Manchester United"
-];
-console.log(zlatanArray.pop());
+const whichPosition = new RugbyPlayer(6.2, 250, 20, true);
 
-//console.log(theManTheMythTheLegend);
-//console.log(theManTheMythTheLegend.fname());
-//console.log(theManTheMythTheLegend.lname());
-//console.log(theManTheMythTheLegend.career());
+const amazingPlayer = new RugbyPlayer(6.5, 260, 20, true);
+
+console.log(amazingPlayer.backPosition);
