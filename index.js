71ec03a4531044c2297a53cf34ac44d2d66cffdb
George Blezard <gblezard91@gmail.com>
2019-11-26 20:33:13 -0600

    Add map superpower array

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const strings = ["Hello", "World", "AgAiN"];
 const numbersAndStrings = numbers.concat(strings);
 //Arrow syntax does not have any THIS reference.
 
+const results = numbers.map(number => {
+  return number + 1;
+});
+
+console.log(numbers);
+console.log(results);
+
 numbers.forEach(number => {
   console.log("Number is", number);
 });
@@ -13,49 +20,53 @@ strings.forEach(function(string) {
 });
 
 function updatedNumbersAndStrings(originalArr, stringUpdate, numberUpdate) {
-  const updatedNumbersAndStrings = [];
-
-  originalArr.forEach(el => {
-    console.log("Current Element is", el);
-
+  return originalArr.map(el => {
     if (typeof el === "string") {
-      updatedNumbersAndStrings.push((el += stringUpdate));
-    } else {
-      updatedNumbersAndStrings.push((el += numberUpdate));
+      return (el += stringUpdate);
     }
+
+    return (el += numberUpdate);
   });
-
-  for (let i = 0; i < originalArr.length; i += 1) {
-    if (typeof originalArr[i] === "string") {
-      updatedNumbersAndStrings.push((originalArr[i] += stringUpdate));
-    } else {
-      updatedNumbersAndStrings.push((originalArr[i] += numberUpdate));
-    }
-  }
-  return updatedNumbersAndStrings;
 }
 
-console.log(updatedNumbersAndStrings(numbersAndStrings, "Hello", 236));
+//   originalArr.forEach(el => {
+//     console.log("Current Element is", el);
 
-/* const updatedNumbersAndStrings = [];
+//     if (typeof el === "string") {
+//       updatedNumbersAndStrings.push((el += stringUpdate));
+//     } else {
+//       updatedNumbersAndStrings.push((el += numberUpdate));
+//     }
+//   });
 
-while (numbersAndStrings.length > 0) {
-  // remove the last element of the array
-  const value = numbersAndStrings.pop();
-  console.log(`${numbersAndStrings.length}: ${value}`);
+//   for (let i = 0; i < originalArr.length; i += 1) {
+//     if (typeof originalArr[i] === "string") {
+//       updatedNumbersAndStrings.push((originalArr[i] += stringUpdate));
+//     } else {
+//       updatedNumbersAndStrings.push((originalArr[i] += numberUpdate));
+//     }
+//   }
+//   return updatedNumbersAndStrings;
 
-  // determine if value is a string or number
-  // and put it in its appropriate array
-  if (typeof value === "string") {
-    strings.push(value);
-  }
-  if (typeof value === "number") {
-    numbers.push(value);
-  }
-}
+// console.log(updatedNumbersAndStrings(numbersAndStrings, "Hello", 236));
 
-console.log("numbers is", numbers);
-console.log("strings is", strings);
-console.log("numbersAndStrings is", numbersAndStrings);
+// /* const updatedNumbersAndStrings = [];
 
-*/
+// while (numbersAndStrings.length > 0) {
+//   // remove the last element of the array
+//   const value = numbersAndStrings.pop();
+//   console.log(`${numbersAndStrings.length}: ${value}`);
+
+//   // determine if value is a string or number
+//   // and put it in its appropriate array
+//   if (typeof value === "string") {
+//     strings.push(value);
+//   }
+//   if (typeof value === "number") {
+//     numbers.push(value);
+//   }
+// }
+
+// console.log("numbers is", numbers);
+// console.log("strings is", strings);
+// console.log("numbersAndStrings is", numbersAndStrings);
