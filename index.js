8147e724ccb37a3ee0803da23890b12a7c560552
George Blezard <gblezard91@gmail.com>
2019-12-02 20:55:15 -0600

    Add Array with map and function

--- index.js
+++ index.js
@@ -1,52 +1,9 @@
-class CenterBack {
-  constructor(fname, lname, age, height, weight) {
-    this.fname = fname;
-    this.lname = lname;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-  }
+const names = ["Misra", "Stevens", "Jamieson", "Wright", "Smith"];
 
-  get fullName() {
-    return `${this.fname} ${this.lname}`;
-  }
-  get size() {
-    return `${this.height} ${this.weight}`;
-  }
-}
-const human1 = new CenterBack("Harry", "Maguire", 26, 6.4, 220);
+const mrNames = names.map(name => `Mr. ${name}`);
 
-console.log(human1);
-
-class BigBack extends CenterBack {
-  constructor(fname, lname, age, height, weight, club, nationality) {
-    super(fname, lname, age, height, weight);
-    this.badge = club;
-    this.homeland = nationality;
-  }
-
-  get nationality() {
-    return `${this.homeland}`;
-  }
-
-  set nationality(homeland) {
-    this.homeland;
-
-    this.nationality = homeland;
-  }
-
-  get club() {
-    return `${this.badge}`;
-  }
-  set club(badge) {
-    this.badge;
-
-    this.club = badge;
-  }
+function addSalutation(names, sal) {
+  return names.map(name => `${sal} ${name}`);
 }
 
-const defender = new CenterBack("Harry", "Maguire", 26, 6.4, 220);
-defender.club = "Man United";
-defender.homeland = "England";
-
-console.log(defender);
+console.log(addSalutation(names, "Mrs."));
