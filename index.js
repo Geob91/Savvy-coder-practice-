4430ae67cea7fd47541743ef53198cb0abdb3db5
George Blezard <gblezard91@gmail.com>
2019-12-05 20:35:26 -0600

    Add extra superpower

--- index.js
+++ index.js
@@ -16,9 +16,18 @@ function doubleAndAdd1(n) {
   return add1(double(n));
 }
 //
-//TODO:Add 1 and double each num inside of nums
-const numArray = nums.map(anyNameIwant => add1AndDouble(anyNameIwant));
-console.log(numArray);
+// //TODO:Add 1 and double each num inside of nums
+// const numArray = nums.map(anyNameIwant => add1AndDouble(anyNameIwant));
+// console.log(numArray);
 
-//TODO: add 1 and double each num inside of nums only using the first 2fxns
-console.log(nums.map(num => add1(num)).map(num => double(num)));
+// //TODO: add 1 and double each num inside of nums only using the first 2fxns
+// console.log(nums.map(num => add1(num)).map(num => double(num)));
+
+//TODO FILTER out all the odd numbers and then repeat
+
+console.log(
+  nums
+    .filter(num => num % 2 === 0)
+    .map(num => add1(num))
+    .map(num => double(num))
+);
