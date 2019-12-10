ccac2cd3fa18e024ea325e3b3282c9a06e6929cb
George Blezard <gblezard91@gmail.com>
2019-12-09 19:57:20 -0600

    Add .includes

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const strs = [
+  "Hello",
+  "something",
+  3,
+  "boolean",
+  "Hello",
+  "Hello",
+  "Hell",
+  "world",
+  "Hello"
+];
 
-/**
- * TODO: Write a fxn. that grabs all of the odd elements in any ARRAY of NUMBERS.
- * HINT: An odd number, num, will RETURN TRUE when we test with: num % 2 === 1;
- */
-// Nums the named parameter doesn't directly reference nums in the global scope
-// => replaces function keyword, braces and return keyword (if it's 1 statement)
+function giveMeAnswer(strings) {
+  return strings.map(strings => {
+    return strings === "Hello";
+  });
+}
+//console.log(giveMeAnswer(strs));
 
-function filterOdds(nums) {
-  return nums.filter(num => num % 2 === 1);
+function hasHello(strings) {
+  return strings
+    .map(string => {
+      return string === "Hello";
+    })
+    .filter(bool => bool).length;
 }
 
-console.log(filterOdds(nums));
-
-// TODO: Write a funtion that returns the sum of all the numbers in any ARRAY of NUMBERS.
-function numberSummer(nums) {
-  return nums.reduce((total, currentNum) => total + currentNum);
-}
-
-console.log(numberSummer(nums));
-
-// TODO: MAP over nums and triple each number.
-
-function timesThree(nums) {
-  return nums.map(num => num * 3);
-}
-
-console.log(timesThree(nums));
+console.log(hasHello(strs));
+//.Includes does everything above --- don't type out above but that is the function of includes.
+console.log(strs.includes("Hello"));
