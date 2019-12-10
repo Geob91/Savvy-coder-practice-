f94273d26364bdd5cdf750eff24db419f86b4ddf
George Blezard <gblezard91@gmail.com>
2019-12-09 20:20:53 -0600

    Add some more examples  with target value length

--- index.js
+++ index.js
@@ -10,21 +10,28 @@ const strs = [
   "Hello"
 ];
 
-function giveMeAnswer(strings) {
-  return strings.map(strings => {
-    return strings === "Hello";
-  });
-}
-//console.log(giveMeAnswer(strs));
+// function giveMeAnswer(strings) {
+//   return strings.map(strings => {
+//     return strings === "Hello";
+//   });
+// }
+// //console.log(giveMeAnswer(strs));
 
-function hasHello(strings) {
-  return strings
-    .map(string => {
-      return string === "Hello";
-    })
-    .filter(bool => bool).length;
+// function hasHello(strings) {
+//   return strings
+//     .map(string => {
+//       return string === "Hello";
+//     })
+//     .filter(bool => bool).length;
+// }
+
+function findValue(someArr, target) {
+  return someArr.filter(element => element === target).length;
 }
 
-console.log(hasHello(strs));
-//.Includes does everything above --- don't type out above but that is the function of includes.
-console.log(strs.includes("Hello"));
+console.log(findValue(strs, "Hello"));
+
+// console.log(findValue(strs));
+// console.log(hasHello(strs));
+// //.Includes does everything above --- don't type out above but that is the function of includes.
+// console.log(strs.includes("Hello"));
